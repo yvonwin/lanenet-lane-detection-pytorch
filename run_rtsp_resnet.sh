99d1b63d407ff90ce2a301086581a9e63b76e99f
yvonwin <wangkui1212@qq.com>
2021-08-23 18:29:46 +0800

    fix conda source error

--- run_rtsp_resnet.sh
+++ run_rtsp_resnet.sh
@@ -1,2 +1,2 @@
-conda activate pytorch
+source ~/miniconda3/bin/activate pytorch
 python test_lanenet_rtsp.py --model ~/Desktop/models/2021-08-20-00-17-34_epochs360_DeepLabv3+_resnet101_best_model.pth --model_type DeepLabv3+ --rtsp_url rtsp://admin:Zhaodao1234!@192.168.31.63:554//Streaming/Channels/2
