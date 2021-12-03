564d7ccab9aa51721a6bec72f3162dece0362eb3
yvonwin <wangkuixz@gmail.com>
2021-12-03 15:18:26 +0800

    remove nouse

--- run_rtsp_ENet.sh
+++ run_rtsp_ENet.sh
@@ -1,4 +1,4 @@
 #source ~/miniconda3/bin/activate pytorch16
-echo "列车轨道避障系统version-1.0"
+echo "version-1.0"
 # python test_lanenet_rtsp.py --model ../lanenet-pytorch_models/2021-08-18-14-10-53_epochs300_ENet_resnet101_best_model.pth  --model_type ENet --rtsp_url rtsp://admin:Zhaodao1234!@192.168.31.63:554//Streaming/Channels/2
 python test_lanenet_rtsp.py --model ../lanenet-pytorch_models/2021-08-18-14-10-53_epochs300_ENet_resnet101_best_model.pth  --model_type ENet --rtsp_url rtsp://127.0.0.1:8554/mystream
